af607ecab274e2c2e56be899994b5aba819f8c7d
Gabriel Cardoos <gabriel.cardoso010@gmail.com>
2018-05-13 16:02:13 -0300

    first commit

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,11 @@
+console.log(7 / 0.0001)
+console.log(7 / 0)
+
+console.log("10" / 2)
+console.log("10abc" / 5)
+
+console.log("Show!" * 2)
+
+console.log(0.1 + 0.7)
+
+console.log((10.5643).toFixed(2))
---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,7 @@
+let qualquer = 'qualquer'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.100
+console.log(qualquer)
+console.log(typeof qualquer)
